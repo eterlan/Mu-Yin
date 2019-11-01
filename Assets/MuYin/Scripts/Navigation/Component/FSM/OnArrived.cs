2230a4cf0d3d3c1c2b075407a2cc03d42ea043e5
eterlan <703246769@qq.com>
2019-11-01 22:36:02 +0800

    missed some file

---
+++ Assets/MuYin/Scripts/Navigation/Component/FSM/OnArrived.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+
+namespace MuYin.AI.Components.FSM
+{
+    public struct OnArrived : IComponentData
+    {
+    }
+}
---
+++ Assets/MuYin/Scripts/Navigation/Component/FSM/OnNavigationStart.cs
@@ -0,0 +1,10 @@
+using Unity.Entities;
+
+
+namespace MuYin.AI.Components.FSM
+{
+    public struct OnStartNavigation : IComponentData
+    {
+        public int Value;
+    }
+}
